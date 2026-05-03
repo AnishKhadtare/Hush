9994dc43f07a388d23c536b2b2bed0780781b159
AnishKhadtare <108408458+AnishKhadtare@users.noreply.github.com>
2026-05-04 01:21:04 +0530

    v2: moved secrets to env variables

--- demo/src/main/java/com/socialApp/demo/service/AuthService.java
+++ demo/src/main/java/com/socialApp/demo/service/AuthService.java
@@ -13,6 +13,8 @@ import com.socialApp.demo.security.AuthUtil;
 import com.socialApp.demo.utils.LocationUtil;
 import lombok.RequiredArgsConstructor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,10 +32,12 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final AuthUtil authUtil;
     private final LocationUtil locationUtil;
+    private final Logger log = LoggerFactory.getLogger(AuthService.class);
 
     public UserSignUpResponse signUp(UserSignUpRequest userSignUpRequest) {
         boolean userExists = userRepository.findByUsername(userSignUpRequest.username()).isPresent();
         if(userExists){
+            log.warn("User already exists sign up operation failed.");
             throw new BadRequestException("User already exists with username: " + userSignUpRequest.username());
         }
         Users user = userMapper.toUserSignUp(userSignUpRequest);
@@ -41,9 +45,12 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(userSignUpRequest.password()));
 
         userRepository.save(user);
-
+        log.debug("User saved successfully with userId : {}", user.getId());
+        log.debug("Update the location of user as current location of signup");
         locationUtil.configureUserGeoLocation(user, userSignUpRequest.longitude(), userSignUpRequest.latitude());
+        log.debug("Updated the location of user as current location of signup");
 
+        log.info("User successfully signed up");
         return userMapper.toUserSignUpResponse(user);
     }
 
@@ -51,8 +58,11 @@ public class AuthService {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(userSignInRequest.username(), userSignInRequest.password())
         );
+        log.debug("User credentials entered are same as that of user credentials in db");
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        log.debug("Generating token for the user trying to login");
         Users user = customUserDetails.getUser();
+        log.debug("Token generation successfull");
         String token = authUtil.generateToken(user);
         return new UserSignInResponse(token);
     }
